cf01e45a1a9b777cd8534f24a7503369a891c78b
ZEESHAN KHAN <162082741+ZEESHAN8692@users.noreply.github.com>
2025-01-27 19:55:14 +0530

    prectice

---
+++ javaScript/prectice/p3h.js
@@ -0,0 +1,60 @@
+// Normal JS 
+// let a= 10
+// let b = 20
+// console.log(a+b) 
+
+// let a= 10
+// function Hello(){
+//     console.log(a+10)
+// }
+// Hello()
+
+    // function Hello(){
+    //     {
+    //         let a = 10
+    //         console.log(a+10)
+    //     }
+    // }
+    // Hello()
+
+    // const a =[2,4,7,9,3,9,5,9,4,7,9]
+    // const b = a.sort()
+    // console.log(b)
+
+    // const b = a.reverse()
+    // console.log(b)
+
+    // const a =["Zeeshan","Khan","Devansh"]
+
+    // for (let i = 0; i < a.length; i++) {
+    //     console.log(a[i])
+        
+    // }
+
+
+
+    // Rest Parameter
+
+    // function Hello(...args){
+    //     console.log(args);
+        
+    // }
+    // Hello(1,2,3,4,5,6)
+
+    // spead operator 
+    // const a = [1,2,3,4,5]
+    // const b = [...a,6,7,8]
+    // console.log(b);
+    
+
+    // concat 
+
+    let a = "Hello"
+    let b = "Zeeshan" 
+    let c = {...a,...b}
+      // let c = a.concat(b)
+    console.log(c);
+
+
+
+
