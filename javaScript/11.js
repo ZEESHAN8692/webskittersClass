b5b0e71f77c6c20b782dcc5eb975e18d09366459
ZEESHAN KHAN <162082741+ZEESHAN8692@users.noreply.github.com>
2025-01-29 22:25:46 +0530

    Prectice p4h

--- javaScript/11.js
+++ javaScript/11.js
@@ -45,3 +45,30 @@
 // console.log(c.age)
 
 
+const js = (name) => {
+    if (name === "Nill") {
+      return function (topic) {
+        console.log(` Hi ${name}. What is Course? ${topic}`);
+      };
+    }
+  
+    if (name === "Raj") {
+      return function (topic) {
+        console.log(` Hi ${name}. What is Course? ${topic}`);
+      };
+    }
+  
+    if (name === "Jhon") {
+      return function (topic) {
+        console.log(` Hi ${name}. What is Course? ${topic}`);
+      };
+    } else {
+      return function () {
+        console.log("Error");
+      };
+    }
+  };
+  
+  js("Nillnn")("React");
+  js("Raj")("Node");
+  js("Jhon")("Angular");
---
+++ javaScript/prectice/p4h.js
@@ -0,0 +1,69 @@
+// function Hi(){
+//     console.log("Hi..");
+    
+// }
+// function HelloBoy(){
+//     console.log("Hello Boy..");
+// }
+// function Hello(a, b , callBack){
+    
+//     console.log(a+b);
+//     callBack()
+
+// }
+// Hello(10,10 , Hi)
+// Hello(10,50, HelloBoy)
+
+// -----------------------------------
+//  Array De-stucturing  
+
+// const a = [1,2,3,45,66,7]
+// const [b,c,d,e,]=a
+// console.log(b,c,d,e)
+
+
+// Object Destucturing
+
+// const b ={
+//     name:"Rahul",
+//     age:25,
+//     gender:"Male",
+// }
+// const {name:rahul, age, gender}=b
+// console.log(rahul,age , gender)
+
+// const a= [
+//     {
+//         name:"rahul" ,
+//         age :25,
+//         gender:"Male",
+//     },
+//     [
+//         {
+//             name:"Zeeshan" ,
+//             age : 23 ,
+//             gender:"Male",
+//         },
+//     ],
+//     {
+//         name:"Rahul" ,
+//         age : 25 ,
+//         gender:["Male","Female","custom"],
+//     }
+// ]
+
+// const [first,second,third] = a
+
+// const {name,age,gender} = first
+// console.log(name);
+// const [ar]=second
+// console.log(ar);
+// const {name:zeeshan, age:age25,gender:Male}=ar
+// console.log(Male);
+// const {name:rahul,age:age1,gender:gender1}=third
+// console.log(gender1);
+// for (const i of gender1) {
+//     console.log(i);
+    
+// }
+
