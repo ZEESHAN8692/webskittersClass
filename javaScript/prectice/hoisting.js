94221d06385b7a9db027c26b92fb6364caee7772
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-02-16 15:33:17 +0530

    hoisting Prectice

---
+++ javaScript/prectice/hoisting.js
@@ -0,0 +1,38 @@
+// clouser
+// function One() {
+//   const name = "Zeeshan";
+//   console.log(20);
+//   function Two() {
+//     const sname = "Khan";
+//     console.log(name);
+//   }
+//   //   console.log(sname);
+//   Two();
+// }
+// One();
+
+// same work if condition
+
+// if (true) {
+//   const name = "Zeeshan";
+//   if (name === "Zeeshan") {
+//     const website = " youtub";
+//     console.log(name + website);
+//   }
+//   //   console.log(website);
+// }
+// // console.log(name);
+
+// ======= interesting ======
+
+
+// console.log(One(10));
+
+// function One(num) {
+//   return num + 10;
+// }
+
+// Two(20);
+// const Two = function (num2) {
+//   return num2 + 20;
+// };
