ec72c4dd697fcb40294f19e46f2f5da40b1936d5
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-02-22 00:46:15 +0530

    Some Prectice

--- javaScript/prectice/p10.js
+++ javaScript/prectice/p10.js
@@ -1,32 +1,179 @@
 // ### Arrays
 // 1. How do you shuffle an array randomly in JavaScript?
 
-let aar = [1, 2, 3, 4, 5, 6];
-aar[aar.length] = 10;
+// const shuffle = (arr) => {
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     let random = Math.floor(Math.random() * (i + 1));
 
-console.log(aar);
+//     let tem = arr[i];
+//     arr[i] = arr[random];
+//     arr[random] = tem;
+//   }
+//   return arr;
+// };
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(shuffle(arr));
+//--------------------------------------------------------
 
-// console.log(b);
 // 2. How do you remove falsy values (like `null`, `undefined`, `0`, `""`, `NaN`, `false`) from an array?
+
+// let arr = [null, undefined, 0, " ", NaN, false, 25, 30];
+
+// let fltr = arr.filter((item) => {
+//   return (
+//     item != NaN &&
+//     item != undefined &&
+//     item != 0 &&
+//     item != " " &&
+//     item != false &&
+//     item != null &&
+//     item
+//   );
+// });
+// console.log(fltr);
+//------------------------------------------------------------
+
 // 3. How do you check if two arrays are equal in JavaScript?
+
+// const equal = (arr1, arr2) => {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// let a = [1, 2, 3, 4];
+// let b = [1, 2, 3, 4];
+// let c = [4, 3, 2, 1];
+
+// console.log(equal(a, c));
+
+//--------------------------------------------
+
 // 4. What is the difference between `Array.includes()` and `Array.indexOf()`?
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log(arr.includes(9)); // check karta hai ki value array ke ander hai ya nahi
+
+// console.log(arr.indexOf(3));    /// ye value ka index check karta hai
+
 // 5. How do you convert an array of objects into a single object using `.reduce()`?
 
-// ---
+//--------------------------------------------------------
 
-// ### Strings
 // 6. How do you capitalize the first letter of each word in a string?
+
+// const capitalize = (str) => {
+//   return str
+//     .split(" ")
+//     .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+//     .join(" ");
+// };
+// let a = capitalize("my name zeeshan");
+// console.log(a);
+
+// const capitstr = (str) => {
+//   return str
+//     .split(" ")
+//     .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+//     .join(" ");
+// };
+// console.log(capitstr("hello my name is zeeshan"));
+
+//---------------------------------------------
+
 // 7. How do you check if a string contains only numbers?
+
 // 8. How do you extract numbers from a string?
+
+// let strt = "h1llo32 ljsa32h8fsdn o9fe  9w843  byu7";
+// let a = strt.split(" ");
+// let num = [];
+// a.forEach((item) => {
+//   let number = item.match(/\d+/);
+//   if (number) {
+//     num.push(number[0]);
+//   }
+// });
+// console.log(num);
+
+// const extract = (str) => {
+//   let number = str.split(" ");
+//   let num = [];
+//   number.forEach((item) => {
+//     let numbers = item.match(/\d+/);
+//     if (numbers) {
+//       num.push(numbers[0]);
+//     }
+//   });
+//   console.log(num);
+// };
+// extract("23dns8d sdf843 88df 65");
+
+//-----------------------------------------------------------------
+
 // 9. What is the difference between `charAt()` and `charCodeAt()`?
+
+// let a = "My Name is Zeeshan";
+// let b = a.charAt(0); // charAt ka kaam string me kisi ek leter ko nikal sakte ho
+// let c = a.charCodeAt(0); // charCodeAt ka kaam keybord ke key ka code nikalna hota hai
+// console.log(c);
+
+//-----------------------------------------------
+
 // 10. How do you replace all occurrences of a word in a string without using `.replaceAll()`?
 
-// ---
+// let a = "hello world";
+// let b = a.replace(/l/g, "b");
+// console.log(b);
+
+// ----------------------------------------
 
 // ### Objects
+
 // 11. How do you find the difference between two objects in JavaScript?
+// let obj1 = {
+//   name: "zeehan",
+//   age: 25,
+// };
+// let obj2 = {
+//   name: "zeehan",
+//   age: 25,
+//   gender: "Male",
+// };
+// let b = obj1.filter((item) => !obj2.includes(item));
+// console.log(b);
+//--------------------------------------------------------
+
 // 12. How do you convert an object into an array of key-value pairs?
+
+// let obj1 = {
+//   name: "zeehan",
+//   age: 25,
+// };
+
+// let b = Object.entries(obj1);
+// console.log(b);
+
 // 13. How do you merge multiple objects dynamically?
+
+// let obj1 = { name: "John" };
+// let obj2 = { age: 30 };
+// let obj3 = { city: "New York" };
+
+// let mergedObj = Object.assign({}, obj1, obj2, obj3);
+// let mergedObject = { ...obj1, ...obj2, ...obj3, gender: "Male" };
+// mergedObject.address = "Bareilly";
+// console.log(mergedObject);
+
+//-------------------------------------------------------
+
 // 14. How do you prevent modification of an object in JavaScript?
 // 15. How do you remove a key from an object without mutating the original object?
 
@@ -76,3 +223,7 @@ console.log(aar);
 // 40. How do you handle multiple asynchronous conditions inside an `if` statement?
 
 // ---
+
+// let aar = [1, 2, 3, 4, 5, 6];
+// aar[aar.length] = 10;
+// console.log(aar);
---
+++ javaScript/prectice/p11h.js
@@ -0,0 +1,22 @@
+// string Mathods
+//1. length
+let a = "My Name is Zeeshan";
+let aa = "   My Name is Zeeshan  ";
+let b = a.length;
+let c = a.toUpperCase();
+let d = a.toLowerCase();
+let e = a.includes("is");
+let f = a.startsWith("My"); // start ka word ya leter check karta hai
+let g = a.endsWith("n"); // end ka word ya leter check karta hai
+let h = a.search("is"); // valuse ka index de deta hai nahi milega to -1
+let i = a.match(/a/g); // ye array banata hai jo bhi leter ya word nikalna hai
+let j = a.indexOf("a"); // indexOf first value ka index deta hai
+let k = a.lastIndexOf("a"); // lastIndexOf last value ka index deta hai
+let l = a.replace(/a/g, "l"); //  "/a/g (globaly search karta hai)"
+let m = aa.trim(); // space remove karta hai
+let n = a.charAt(0); // ye index ka word return kar deta hai
+let o = a.charCodeAt(0); // ye keyboard ki key ka code deta hai
+let p = String.fromCharCode(70); // ye key code ka key deta hai
+let q = a.repeat(2); // ye jo string hoti hai repeat karta rahta hai jitni bolo
+let r = a.substring(0, 5); //  ye last index ko count nahi karta hai
+console.log(j);
