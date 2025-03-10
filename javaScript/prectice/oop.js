30bb6ed5ae2b8f7d656d92975f5a3373651726a7
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-03-10 20:26:13 +0530

    prectice

--- javaScript/prectice/oop.js
+++ javaScript/prectice/oop.js
@@ -1,3 +1,18 @@
+// constructor function
+
+function user(userName, login, isLogin) {
+  this.userName = userName;
+  this.login = login;
+  this.isLogin = isLogin;
+  return this;
+}
+
+// const userOne = user("zeeshan", 11, true);
+// const userTwo = user("khan", 11, true);
+const userOne = new user("zeeshan", 11, true);
+const userTwo = new user("khan", 11, true);
+console.log(userTwo);
+
 // class Hello {
 //   zeeshan() {
 //     console.log("Hello Zeeshan");
@@ -16,23 +31,23 @@
 // 2.  prototype
 // 3. static
 
-class Hello {
-  constructor(name, age) {
-    this.anisName = name;
-    this.anisAge = age;
-    console.log("hello zeeshan");
-  }
-  anis() {
-    console.log(`Hello ${this.anisName} and age ${this.anisAge}`);
-  }
-  static zeeshan() {
-    console.log("Hello ZEESHAN KHAN");
-  }
-}
+// class Hello {
+//   constructor(name, age) {
+//     this.anisName = name;
+//     this.anisAge = age;
+//     console.log("hello zeeshan");
+//   }
+//   anis() {
+//     console.log(`Hello ${this.anisName} and age ${this.anisAge}`);
+//   }
+//   static zeeshan() {
+//     console.log("Hello ZEESHAN KHAN");
+//   }
+// }
 
-let a = new Hello("anis", 25);
-let b = new Hello("Zeeshan Khan", 23);
-a.anis();
-b.anis();
+// let a = new Hello("anis", 25);
+// let b = new Hello("Zeeshan Khan", 23);
+// a.anis();
+// b.anis();
 
-Hello.zeeshan();
+// Hello.zeeshan();
