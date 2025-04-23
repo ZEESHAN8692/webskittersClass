99b87fb04a475d7a18193e1d01fc717336ee1cff
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-04-23 12:09:49 +0530

    prectice

--- react/project4/src/API Like Data/carouselData.js
+++ react/project4/src/API Like Data/carouselData.js
@@ -4,23 +4,23 @@ export default [
     imageUrl:
       "https://img.freepik.com/premium-psd/psd-black-friday-background_1028932-146.jpg?w=826",
     altText: "first Slider",
-    heading: "Hello Suman",
-    description: "Hello World",
+    heading: "Best Fashion Design For Girls in India",
+    description: "Best Fashion Design For Girls",
   },
   {
     id: 2,
     imageUrl:
       "https://img.freepik.com/premium-photo/fashionable-woman-holding-jacket-looking-away-isolated-dark-blue-ai-generated-image_548188-10548.jpg?w=826",
     altText: "second slider",
-    heading: "Hello Sneha ",
-    description: "Hello World",
+    heading: "Best Fashion Design For Girls in India",
+    description: "Best Fashion Design For Girls",
   },
   {
     id: 3,
     imageUrl:
       "https://img.freepik.com/premium-psd/psd-black-friday-background_1028932-146.jpg?w=826",
     altText: "Second Slider",
-    heading: "Hello Zeeshan",
-    description: "Hello World",
+    heading: "Best Fashion Design For Girls in India",
+    description: "Best Fashion Design For Girls",
   },
 ];
