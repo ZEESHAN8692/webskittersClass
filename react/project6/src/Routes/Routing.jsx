756c8192cd2de991e6f38dd623ba11b4343d07f9
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-04-28 12:03:14 +0530

    prectice

---
+++ react/project6/src/Routes/Routing.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import PageNotFound from "../Pages/pageNotFound";
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+};
+
+export default Routing;
