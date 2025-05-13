f90b722de4c0ae874779f279ef35699c0594efae
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-05-13 21:35:48 +0530

    complete home work

--- react/project6/src/Pages/Details.jsx
+++ react/project6/src/Pages/Details.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Base_Url from "../Api/Base_Url";
 import { Users_end } from "../Api/End_point";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Details = () => {
   const { id } = useParams();
-// let apiUrl=Base_Url+Users_end+id;
+  let apiUrl = Base_Url + Users_end + id;
+  const [singleData, setSingleData] = useState({});
+  const getSingleData = () => {
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        setSingleData(res.data);
+        console.log(res.data);
+      })
+      .then((err) => console.log("Error", err));
+  };
+  useEffect(() => {
+    getSingleData();
+  }, []);
+  // useEffect(() => {
+  //   getSingleData();
+  // }, [singleData]);
   return (
     <>
-      <div>
-        <h1>Details Page </h1>
-        <h1>{id}</h1>
+      <div className="container m-auto mt-5">
+        <Card>
+          <Card.Header as="h5">{singleData.username}</Card.Header>
+          <Card.Body>
+            <Card.Title>{singleData.name}</Card.Title>
+            <Card.Text>Email :- {singleData.email}</Card.Text>
+            <Card.Text>Phone :- {singleData.phone}</Card.Text>
+            <Card.Text>
+              Address :- {singleData?.address?.street}
+              {singleData?.address?.suite}
+              {singleData?.address?.city}
+            </Card.Text>
+            <Card.Text>{singleData?.address?.pincode} </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     </>
   );
