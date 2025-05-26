94fad06fbcc62cc188b9921dcff0c6e6f558c841
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-05-26 13:18:04 +0530

    prectice

--- nodejs/nodejsbasic/app.js
+++ nodejs/nodejsbasic/app.js
@@ -1,39 +1,84 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+const { error } = require("console");
 
 // console.log(path);
 
-// http.createServer((req,res)=>{
-//     res.write('hello world')
-//     res.end()
-// }).listen(3005)
+// http
+//   .createServer((req, res) => {
+//     res.write("hello world");
+//     res.end();
+//   })
+//   .listen(3005);
+
+// http
+//   .createServer((req, res) => {
+//     fs.readFile("hello.html", (err, data) => {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     });
+//     // fs.appendFile("zeeshan.txt", "Hello Zeeshan", (err) => {
+//     //   if (err) throw err;
+//     //   console.log("saved");
+//     // });
+
+//     // fs.appendFile("zeeshan.txt", "This is my text", (err) => {
+//     //   if (err) throw err;
+//     //   console.log("Updated");
+//     // });
+
+//     // fs.unlink("zeeshan.txt", (err) => {
+//     //   if (err) throw err;
+//     //   console.log("Deleted");
+//     // });
+
+//     fs.rename("zeeshan.txt", "zeeshankhan.txt", (err) => {
+//       if (err) throw err;
+//       console.log("file name change");
+//     });
+//   })
+//   .listen(5000);
+
+// http
+//   .createServer((req, res) => {
+//     res.write("Hello World");
+//     res.end();
+//   })
+//   .listen(3000);
 
 http
   .createServer((req, res) => {
     fs.readFile("hello.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
+      res.writeHead(200, { "content-type": "text/html" });
       res.write(data);
-      return res.end();
+      res.end();
     });
-    // fs.appendFile("zeeshan.txt", "Hello Zeeshan", (err) => {
-    //   if (err) throw err;
-    //   console.log("saved");
+
+    // Create File
+    // fs.appendFile("hello.txt", "This is text text file", (err) => {
+    //   if (err) throw error;
+    //   console.log("File Save");
     // });
 
-    // fs.appendFile("zeeshan.txt", "This is my text", (err) => {
+    // Add text file
+
+    // fs.appendFile("hello.txt", "Hello  this size Zeeshan ", (err) => {
     //   if (err) throw err;
-    //   console.log("Updated");
+    //   console.log("File Update ");
     // });
 
-    // fs.unlink("zeeshan.txt", (err) => {
+    // Rename file name
+
+    // fs.rename("hello.txt", "hello1.txt", (err) => {
     //   if (err) throw err;
-    //   console.log("Deleted");
+    //   console.log("File name Change ");
     // });
 
-    fs.rename("zeeshan.txt", "zeeshankhan.txt", (err) => {
+    fs.unlink("zeeshankhan.txt", (err) => {
       if (err) throw err;
-      console.log("file name change");
+      console.log("File deleted");
     });
   })
   .listen(5000);
