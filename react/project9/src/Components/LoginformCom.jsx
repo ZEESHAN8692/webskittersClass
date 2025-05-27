98600d76e3e8f2c499707533de9f8b37c40339c2
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-05-28 00:22:23 +0530

    react home work

--- react/project9/src/Components/LoginformCom.jsx
+++ react/project9/src/Components/LoginformCom.jsx
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import base_url from "../../Api/base_url";
+import { login_end } from "../../Api/end_point";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginformCom = () => {
+  const navigater = useNavigate();
+  const apiUrl = base_url + login_end;
   const [input, setInput] = useState({
     email: "",
     password: "",
   });
 
   const handleInput = (e) => {
-
     const { name, value } = e.target;
     setInput({ ...input, [name]: value });
   };
 
   const handleSub = (e) => {
     e.preventDefault();
+    axios
+      .post(apiUrl, input)
+      .then((res) => {
+        let { token, data } = res.data;
+        
+        if (res.status === 200) {
+          alert("Login Successfully");
+          sessionStorage.setItem("token", token);
+          sessionStorage.setItem("name", data.first_name);
+          navigater("/dashboard");
+        } else {
+          console.log(res);
+        }
+      })
+      .catch((err) => console.log(err));
 
     console.log("Form Data ", input);
   };
--- react/project9/src/Pages/Dashboard.jsx
+++ react/project9/src/Pages/Dashboard.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const loginUser = sessionStorage.getItem("name");
+  const navigater = useNavigate();
+  const token = sessionStorage.getItem("token");
+  const logout = () => {
+    window.confirm("Are you sure");
+    navigater("/login-form");
+    sessionStorage.removeItem("name");
+    sessionStorage.removeItem("token");
+  };
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <div className="d-flex justify-content-end gap-5 align-items-center container">
+        {loginUser && (
+          <div>
+            <h1>Hello , {loginUser}</h1>
+          </div>
+        )}
+
+        <div>
+          {token && (
+            <Button variant="primary" onClick={logout}>
+              Logout
+            </Button>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
