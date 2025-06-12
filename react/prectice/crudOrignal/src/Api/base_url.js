63b336b042f2de0c78a9a84f0fa8cb0b070a58af
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-06-12 23:05:01 +0530

    React Work

---
+++ react/prectice/crudOrignal/src/Api/base_url.js
@@ -0,0 +1,2 @@
+const base_url = "https://wtsacademy.dedicateddevelopers.us/api/";
+export default base_url;
---
+++ react/prectice/crudOrignal/src/Api/end_point.js
@@ -0,0 +1,5 @@
+const login_end = "user/signin";
+const signup_end = "user/signup";
+const profile_end = "user/profile-details";
+
+export { login_end, signup_end, profile_end };
