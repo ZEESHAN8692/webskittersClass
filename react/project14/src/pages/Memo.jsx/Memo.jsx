f71923438423351cb7c4515f0f1badc1abf4705b
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-06-26 00:27:36 +0530

    prectice

--- react/project14/src/pages/Memo.jsx/Memo.jsx
+++ react/project14/src/pages/Memo.jsx/Memo.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useMemo, useState } from "react";
 
 const Memo = () => {
-  
-  return (
-    <div>Memo</div>
-  )
-}
+  const [count, setCount] = useState(0);
 
-export default Memo
+  const sum = () => {
+    console.log("Counting Sum ");
+    let result = 0;
+    for (let i = 0; i < 100000; i++) {
+      result += i;
+    }
+    return result;
+  };
+  // const sumValue = sum();
+  const sumValue = useMemo(() => sum(), []);
+
+  return (
+    <>
+      <div className="container">
+        <h2>sum :- {sumValue}</h2>
+
+        <h1>{count}</h1>
+        <br />
+        <button onClick={() => setCount(count + 1)}>Increase Count </button>
+      </div>
+    </>
+  );
+};
+
+export default Memo;
