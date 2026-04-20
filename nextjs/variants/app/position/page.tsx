7a0e207586326e65884bfc5c25a3e807104dac8a
ZEESHAN KHAN <162082741+ZEESHAN8692@users.noreply.github.com>
2026-04-20 23:05:14 +0530

    prectice css

---
+++ nextjs/variants/app/position/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const PositionPage = () => {
+  return (
+    <>
+        <div className="main">
+            <div className="box1">1</div>
+            <div className="box2">2</div>
+            <div className="box3">3</div>
+            <div className="box4">4</div>
+        </div>
+        <p className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, esse libero. Error, laboriosam, voluptas ab, fuga quam ea voluptatem eos odit numquam facilis magni reiciendis! Sit voluptate accusamus eveniet doloremque!
+        Dolorum, magnam optio expedita recusandae ducimus nemo odio quasi nihil in beatae fuga doloremque maiores porro libero alias, minus soluta quibusdam qui molestiae ullam vero! Praesentium laudantium expedita ullam aut?
+        Ipsam itaque ex molestias est rerum libero cum qui, doloremque quis veniam atque doloribus ipsum, debitis dolorum quasi quo ad illo suscipit autem ab voluptate. Sunt ducimus temporibus blanditiis unde!
+        Voluptatibus suscipit minima eaque consectetur libero quas repellendus sunt totam debitis et autem reprehenderit harum, laudantium accusamus veritatis dolor pariatur optio? Eveniet esse nulla deleniti nemo iste saepe, incidunt veritatis.
+        Temporibus, culpa earum quasi officia qui exercitationem cupiditate impedit ducimus tempora, laborum iste dolore asperiores doloribus harum possimus explicabo, repudiandae optio magnam vero molestias. Similique quis in veniam officiis debitis?
+        Similique, mollitia vero officia officiis quo, excepturi atque alias blanditiis voluptatem libero fuga aperiam nihil? Neque aliquam quasi harum veniam suscipit rem error incidunt in, veritatis illum molestias. Recusandae, excepturi.
+        Deserunt, facere doloremque praesentium nemo delectus voluptatem voluptatum ipsa nostrum, sunt nulla perspiciatis natus sapiente nam. Perferendis quas rerum a qui earum omnis at ea, modi iure illo, officiis maxime?
+        Laborum cupiditate hic animi provident atque culpa modi sit voluptatum. Tenetur, quos expedita deleniti architecto maxime est, nulla, tempora beatae hic voluptatem nisi similique quidem! Illum sint architecto ipsum delectus?
+        Et earum eligendi magni nesciunt? Nulla officia harum repellat recusandae aperiam doloremque dolorem quis molestiae, quaerat cupiditate reprehenderit et enim voluptatum, culpa iure quisquam deserunt dicta id laudantium laboriosam distinctio.
+        Autem facilis illum totam nemo ipsum modi veniam natus tenetur quaerat dicta quas ipsa id, sunt hic voluptate cupiditate molestiae provident similique porro fugit repellat. Esse harum eligendi praesentium ipsum?
+        </p>
+    </>
+  )
+}
+
+export default PositionPage
---
+++ nextjs/variants/app/responsive/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Responsive = () => {
+  return (
+    <>
+      <div className="responsiveDiv">
+          Hello  
+      </div>
+    </>
+  )
+}
+
+export default Responsive
